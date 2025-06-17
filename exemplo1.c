d80cbcec9f74c7c7e3c235238512dd023f4d5a15
Navarro Rangel <navarrorangel1@hotmail.com>
2025-06-17 12:46:21 -0300

    :sparkles: feat: adicionando uma string em C e aprendendeo como fazer o gerenciamento de memória delas

--- exemplo1.c
+++ exemplo1.c
@@ -3,9 +3,14 @@
 int main(){
 	int numero = 10;
 	float numeroFloat = 40.34;
+	char letra = 'A';
+	char nome[8] = "Navarro";
 	
 	printf("Numero: %d \n", numero);
-	printf("Flutuante: %.3f", numeroFloat);
+	printf("Flutuante: %.2f \n", numeroFloat);
+	printf("Letra: %c \n", letra);
+	printf("Nome: %s \n", nome);
+	
 	
 	
 	return 0;
