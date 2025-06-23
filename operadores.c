75e713eb85232115d5cd98acb36210f7d99e2585
Navarro Rangel <navarrorangel1@hotmail.com>
2025-06-23 14:52:31 -0300

    :sparkles: feat: adicionada a divisao

--- operadores.c
+++ operadores.c
@@ -12,9 +12,13 @@ int main (){
 	int resultadoMultiplicacao;
 	resultadoMultiplicacao = num1 * num2;
 	
+	int resultadoDivisao;
+	resultadoDivisao = num1 / num2;
+	
 	printf("Adicao: %d + %d = %d \n", num1, num2, resultadoSoma );
 	printf("Subtracao: %d - %d = %d \n", num1,num2, resultadoSubtracao );
 	printf("Multiplicacao: %d * %d = %d \n", num1,num2,resultadoMultiplicacao);
+	printf("Divisao: %d / %d = %d \n", num1,num2,resultadoDivisao);
 	
 	return 0;
 }
