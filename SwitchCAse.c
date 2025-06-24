371c9ab7c9e30d694a0be9a4874d773c25cea815
Navarro Rangel <navarrorangel1@hotmail.com>
2025-06-24 15:12:07 -0300

    :sparkles: feat: Estrutura padrao de switch case

---
+++ SwitchCAse.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int opcao = 2;
+	switch (opcao){
+		case 1:
+			printf("Fase numero 1");
+			break;
+		case 2:
+			printf("fase numero 2");
+			break;
+		case 3:
+			printf("fase numero 3");
+			break;
+		default:
+			printf("Nao existe!");
+			
+	}
+	
+	return 0;
+}
