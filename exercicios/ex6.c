5090a65c60321574c0eb19914b808c7f88f085be
Navarro Rangel <navarrorangel1@hotmail.com>
2025-06-24 10:02:35 -0300

    :sparkles: feat: exercicio 6 resolvido utilizando if else para verificacao de cartoes em um jogo de futebol

---
+++ exercicios/ex6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (){
+	int cartoesAmarelos;
+	int cartoesVermelhos;
+	
+	printf("Quantos cartoes amarelos esse jogador possui?\n");
+	scanf("%d", &cartoesAmarelos);
+	printf("Esse jogador tem cartao vermelho? digite 1 para caso ele possua ou 0 caso ele nao possua \n");
+	scanf("%d", &cartoesVermelhos);
+	
+	printf("Pensando... \n");
+	
+	if(cartoesAmarelos >= 2 || cartoesVermelhos == 1 ){
+		printf("VOCE FOI EXPULSO, SAIA DO CAMPO!");
+		
+	} else {
+		printf("Cuidado Para nao tomar outro");
+	}
+	
+	return 0;
+}
