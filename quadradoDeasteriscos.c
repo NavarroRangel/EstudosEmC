741fe15682d49cb5aee9eebc5e2dd2dcf62a6fad
Navarro Rangel <navarrorangel1@hotmail.com>
2025-06-26 13:18:34 -0300

    :sparkles: feat: exercicio contador simples

--- quadradoDeasteriscos.c
+++ quadradoDeasteriscos.c
@@ -3,11 +3,11 @@
 int main(){
 	
 	int arestaQuadrado;
+	
 	printf("Digite o quadrado que voce quer!\n");
 	scanf("%d", &arestaQuadrado);
+	
 	for(int i = 0; i <= arestaQuadrado - 1; i++ ){
-		//printf("\n quadrado %d\n", i);
-
 		for(int j = 1; j<= arestaQuadrado ; j++){
 			printf("*");
 		}
