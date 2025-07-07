4002c7c1ed8444919655e9030058cc8724c1406d
Navarro Rangel <navarrorangel1@hotmail.com>
2025-07-07 14:13:56 -0300

    :sparkles: feat: adicionado arrays e como printalos com loops

---
+++ array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	int notas[5] = {10,20,40,30,12};
+	// Int: tipo dos valores
+	// Notas: nome do vetor
+	// [5]: número total de elementos que cabem no vetor.
+	for (int i = 0; i < 5; i++){
+		printf("%d \n",notas[i]);
+	}
+	
+	
+	return 0;
+}
